79745152a2e179e542851cc159473147f83f5ab0
tuna <gtuna52@gmail.com>
2022-04-18 09:53:02 +0300

    spawning gun actor at runtime

--- Source/SimpleShooter/ShooterCharacter.cpp
+++ Source/SimpleShooter/ShooterCharacter.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "Gun.h"
 #include "ShooterCharacter.h"
 
 // Sets default values
@@ -14,6 +14,8 @@ AShooterCharacter::AShooterCharacter()
 void AShooterCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+ 	Gun = GetWorld()->SpawnActor<AGun>(GunClass);
 	
 }
 
