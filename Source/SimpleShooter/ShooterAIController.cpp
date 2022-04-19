e9363b6e7b87c985bc5cfab73c181eed3d8a38ae
tuna <gtuna52@gmail.com>
2022-04-19 12:13:11 +0300

    AI Aiming

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -1,5 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "Kismet/GameplayStatics.h"
 #include "ShooterAIController.h"
 
+
+void AShooterAIController::BeginPlay()
+{
+    Super::BeginPlay();
+    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0); //returns the first player pawn
+    
+    SetFocus(PlayerPawn);
+}
