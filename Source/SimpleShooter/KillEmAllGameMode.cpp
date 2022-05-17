2acd0e06a02394c36c0dc56917d3b517f6973ba5
tuna <gtuna52@gmail.com>
2022-05-17 15:46:38 +0300

    restarting game with EndGame func (lec 217)

--- Source/SimpleShooter/KillEmAllGameMode.cpp
+++ Source/SimpleShooter/KillEmAllGameMode.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "EngineUtils.h"
+#include "GameFramework/Controller.h"
 #include "KillEmAllGameMode.h"
 
 void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
@@ -8,10 +9,18 @@ void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 	Super::PawnKilled(PawnKilled);
 
 	APlayerController* PlayerController = Cast<APlayerController>(PawnKilled->GetController());
-	if ( PlayerController)
+	if (PlayerController)
 	{
-		PlayerController->GameHasEnded(nullptr, true);
+		EndGame(false);
 	}
-
-
 }
+
+void AKillEmAllGameMode::EndGame(bool bIsPlayerWinner)
+{
+	for (AController* Controller : TActorRange<AController>(GetWorld()))
+	{
+		const bool bIsWinner = Controller->IsPlayerController() == bIsPlayerWinner;
+		Controller->GameHasEnded(Controller->GetPawn(), bIsWinner);
+	}
+}
+
